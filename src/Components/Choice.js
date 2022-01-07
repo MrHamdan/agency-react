18f15dd745f15b2ec2489a7c37f8ef50ecee51ad
Staff Asia <dreamteam@iMac.local>
2022-01-07 17:21:38 +0600

    first commit

---
+++ src/Components/Choice.js
@@ -0,0 +1,28 @@
+import { Box, Container, Grid, Typography } from '@mui/material';
+import React from 'react';
+import meeting from '../Images/meeting.png';
+
+
+
+
+const Choice = () => {
+    return (
+        <Container sx={{ marginTop: '150px' }}>
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={2}>
+                    <Grid item xs={8} sm={12} lg={6} md={5}>
+                        <Typography sx={{ fontWeight: 'bold', fontSize: '40px', color: 'white' }}>Why Enver Is The <br /> Best Choice?</Typography>
+                    </Grid>
+                    <Grid item xs={4} sm={12} lg={5} md={5}>
+                        <Typography sx={{ fontWeight: 'bold', fontSize: '14px', color: 'white', marginTop: '30px' }}>Watch this one minute video so you understand why you should <br /> use our services!</Typography>
+                    </Grid>
+                </Grid>
+            </Box>
+            <Box sx={{ textAlign: 'center', marginTop: '40px' }}>
+                <img style={{ width: '100%' }} src={meeting} alt="" />
+            </Box>
+        </Container>
+    );
+};
+
+export default Choice;
